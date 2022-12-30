5029b94234854ee1f171cecc020e45fa0a1c0864
Micael <micaelcapo1316@gmail.com>
2022-12-30 17:12:17 -0300

    ControllerAdvisor to handle Custom Exceptions

--- demo/src/main/java/com/example/NewLibrary17/demo/exception/ControllerAdvisor.java
+++ demo/src/main/java/com/example/NewLibrary17/demo/exception/ControllerAdvisor.java
@@ -54,4 +54,14 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(SoftDeleteException.class)
+    public ResponseEntity<Object> handleSoftDeleteException(SoftDeleteException ex, WebRequest request){
+        Map<String, Object> body = new LinkedHashMap<>();
+
+        body.put("timestamp", LocalDateTime.now());
+        body.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+
 }
